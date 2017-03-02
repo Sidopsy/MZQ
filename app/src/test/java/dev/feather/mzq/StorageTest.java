b56a0ad66d449c4ed7b2d0827e3b0dfdd9c9f4a3
Mattias Landkvist <mattias.landkvist@gmail.com>
2017-03-02 10:58:34 +0100

    Initial commit 2

---
+++ app/src/test/java/dev/feather/mzq/StorageTest.java
@@ -0,0 +1,21 @@
+package dev.feather.mzq;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mattias on 2017-03-02.
+ */
+public class StorageTest {
+    @Test
+    public void store() throws Exception {
+
+    }
+
+    @Test
+    public void retrive() throws Exception {
+
+    }
+
+}
---
+++ app/src/test/java/dev/feather/mzq/TrackTest.java
@@ -0,0 +1,31 @@
+package dev.feather.mzq;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mattias on 2017-03-02.
+ */
+public class TrackTest {
+    @Test
+    public void name() throws Exception {
+
+    }
+
+    @Test
+    public void forward() throws Exception {
+
+    }
+
+    @Test
+    public void backward() throws Exception {
+
+    }
+
+    @Test
+    public void length() throws Exception {
+
+    }
+
+}
