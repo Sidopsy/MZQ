eee8099bb244273485bd82b818ed015b14903370
Mattias Landkvist <mattias.landkvist@gmail.com>
2017-03-02 12:20:01 +0100

    StorageTest initial unit testing done

--- app/src/test/java/dev/feather/mzq/StorageTest.java
+++ app/src/test/java/dev/feather/mzq/StorageTest.java
@@ -1,5 +1,6 @@
 package dev.feather.mzq;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,14 +9,24 @@ import static org.junit.Assert.*;
  * Created by mattias on 2017-03-02.
  */
 public class StorageTest {
+    Storage storage;
+    TrackList trackList;
+
+    @Before
+    public void setup() throws Exception {
+        storage = new StorageImpl();
+        trackList = new TrackList();
+    }
+
     @Test
     public void store() throws Exception {
-
+        assertTrue(storage.store(trackList));
     }
 
     @Test
     public void retrive() throws Exception {
-
+        storage.store(trackList);
+        assertEquals(trackList, storage.retrive());
     }
 
 }
